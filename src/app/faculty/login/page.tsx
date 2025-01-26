81c919f14297818a34d087febc8d44a1dedbe8b9
Aditya Pant <pantanupam05@gmail.com>
2025-01-26 23:24:11 +0530

    login structure

--- src/app/faculty/login/page.tsx
+++ src/app/faculty/login/page.tsx
@@ -1,3 +1,25 @@
+import { Input } from "@/components/ui";
+import { Button } from "@/components/ui";
+import Image from "next/image";
+
 export default function LoginPage() {
-	return <h1>This is login you shit!</h1>;
+	return (
+		<div>
+			<div className="flex flex-col items-center drop-shadow-lg translate-y-1/2 bg-slate-400">
+				<Image
+					src="/logo.png"
+					width={200}
+					height={200}
+					alt="Picture of the author"
+				/>
+				<Input type="email" placeholder="Email" className="w-[300px] mb-2" />
+				<Input
+					type="password"
+					placeholder="password"
+					className="w-[300px] mb-2"
+				/>
+				<Button>Login</Button>
+			</div>
+		</div>
+	);
 }
--- src/app/faculty/page.tsx
+++ src/app/faculty/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
 export default function FacultyLayout() {
-	return <div>FacultyLayout</div>;
+	return;
+	<div></div>;
 }
