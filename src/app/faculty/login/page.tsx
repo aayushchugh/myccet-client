0dd667d17c635357f051ba516dd3c8c2da050e13
Aditya Pant <pantanupam05@gmail.com>
2025-01-27 11:02:16 +0530

    logo edite

--- src/app/faculty/login/page.tsx
+++ src/app/faculty/login/page.tsx
@@ -5,14 +5,14 @@ import Image from "next/image";
 export default function LoginPage() {
 	return (
 		<div>
-			<div className="flex flex-col items-center drop-shadow-lg translate-y-1/2 bg-slate-400">
+			<div className="flex flex-col items-center drop-shadow-lg translate-y-1/2 ">
 				<Image
-					src="/logo.png"
+					src="/logo.svg"
 					width={200}
 					height={200}
 					alt="Picture of the author"
 				/>
-				<Input type="email" placeholder="Email" className="w-[300px] mb-2" />
+				<Input type="email" placeholder="Email" className="w-3/4 mb-2" />
 				<Input
 					type="password"
 					placeholder="password"
