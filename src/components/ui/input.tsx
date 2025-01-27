0c6a2ddf4a2e293180f51d3464e1e4279d9260a7
Ayush Chugh <hey@ayushchugh.com>
2025-01-27 21:43:54 +0530

    chore: prettier config

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -14,10 +14,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 					type={type}
 					className={cn(
 						"flex h-9 w-full rounded-md border bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-						error
-							? "border-red-500 focus-visible:ring-red-500"
-							: "border-input",
-						className
+						error ? "border-red-500 focus-visible:ring-red-500" : "border-input",
+						className,
 					)}
 					ref={ref}
 					{...props}
@@ -25,7 +23,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 				{error && <span style={{ color: "red" }}>{error}</span>}
 			</div>
 		);
-	}
+	},
 );
 Input.displayName = "Input";
 
