8e1cf6e093222588ced851b631a57a1d1f4e0ec8
Ayush Chugh <hey@ayushchugh.com>
2025-01-27 13:54:42 +0530

    feat: update theme color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { Button } from "../components/ui";
+import { Card, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
+
 export default function Home() {
-	return <div></div>;
+	return (
+		<section className="min-h-dvh flex items-center justify-center">
+			<Card className="w-[350px] ">
+				<CardHeader>
+					<CardTitle>
+						<img src="/logo.svg" alt="" />
+					</CardTitle>
+					{/* <CardDescription className="flex items-center">
+            Your Role
+          </CardDescription> */}
+				</CardHeader>
+
+				<CardFooter className="flex justify-between">
+					<Button>Faculty</Button>
+					<Button>Admin</Button>
+				</CardFooter>
+			</Card>
+		</section>
+	);
 }
